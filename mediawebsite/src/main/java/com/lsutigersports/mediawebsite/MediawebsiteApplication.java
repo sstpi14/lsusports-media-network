e104196ded7509781828bf0d01467035157b8500
Stephen St. Pierre <st.pierre.s.98@gmail.com>
2023-08-29 12:14:32 -0400

    committing codebase for backend

---
+++ mediawebsite/src/main/java/com/lsutigersports/mediawebsite/MediawebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.lsutigersports.mediawebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediawebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediawebsiteApplication.class, args);
+	}
+
+}
---
+++ mediawebsite/src/test/java/com/lsutigersports/mediawebsite/MediawebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsutigersports.mediawebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediawebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
